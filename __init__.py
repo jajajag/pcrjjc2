18a3267abd3c1fb48af98b1fa39e4489db305094
Jag Zhang <jajajag@users.noreply.github.com>
2025-06-02 03:23:36 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -188,8 +188,7 @@ async def on_query_clan_name(bot, ev):
                                  at_sender=True)
             # JAG: 如果存在多个含有该名称的公会，按照会长名过滤
             elif len(res['list']) > 1:
-                show_clans = [f"\n{clan['clan_name']}--{clan['leader_name']}" \
-                        for clan in res['list']]
+                show_clans = [f"\n{clan['clan_name']} -> {clan['leader_name']}" for clan in res['list']]
                 show_clans_message = ''.join(show_clans)
                 if leader_name is None:
                     await bot.finish(ev, '找到多个含有该名称的公会，请提供会长名：' + show_clans_message, at_sender=True)
